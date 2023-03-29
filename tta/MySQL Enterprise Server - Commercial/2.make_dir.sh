dfa14083a4d1d7b71f58d778cb3294b6edee7f93
qweqwerq <88891704+qweqwerq@users.noreply.github.com>
2023-03-29 15:57:31 +0900

    Create 2.make_dir.sh

---
+++ tta/MySQL Enterprise Server - Commercial/2.make_dir.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p /home/mysql
+mkdir -p /home/mysql/data
+mkdir -p /home/mysql/temp
+mkdir -p /home/mysql/log
+mkdir -p /home/mysql/binlog
+mkdir -p /home/mysql/etc
+mkdir -p /home/mysql/mysql-latest
+mkdir -p /home/mysql/keyring
+
+# single mysql
+cp /root/tta-main/my.cnf_single /home/mysql/etc/my.cnf
+
+# cluster mysql (when cluster mysql install, remove the comment and then change server id according to assigned id)
+# cp /root/tta-main/my.cnf_cluster /home/mysql/etc/my.cnf
+
+cp /root/tta-main/4.start_mysql.sh /home/mysql/start_mysql.sh
+cp /root/tta-main/4.stop_mysql.sh /home/mysql/stop_mysql.sh
+chmod 750 /home/mysql/start_mysql.sh
+chmod 750 /home/mysql/stop_mysql.sh
+
+chown -R mysql:mysql /home/mysql
+echo "export PATH=/home/mysql/mysql-latest/bin:\$PATH" >> /etc/profile
+echo "export PATH=/home/mysql/mysql-latest/bin:\$PATH" >> ~/.bashrc
+
+source /etc/profile
+. ~/.bashrc
